609cd99c7374756aaf4b00b2a1962392729ff2c4
Alexandru Oglinda <ogycat2021@icloud.com>
2021-04-04 14:57:20 +0300

    added a for loop

--- src/com/company/homework0304/BarChartPrintProgram.java
+++ src/com/company/homework0304/BarChartPrintProgram.java
@@ -2,6 +2,9 @@ package com.company.homework0304;
 
 public class BarChartPrintProgram {
     public static void main(String[] args) {
-
+        System.out.println("To be added later");
+        for (int i = 0; i < 100; i++) {
+            System.out.println(i);
+        }
     }
 }
